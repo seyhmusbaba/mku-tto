03158de098394305c74839698140d6e936d8f37a
Şeyhmus Gürkan <64413869+seyhmusbaba@users.noreply.github.com>
2026-03-14 19:27:04 +0300

    Update analytics.module.ts

--- backend/src/analytics/analytics.module.ts
+++ backend/src/analytics/analytics.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from '../database/entities/project.entity';
 import { User } from '../database/entities/user.entity';
 import { ProjectReport } from '../database/entities/project-report.entity';
+import { SystemSetting } from '../database/entities/system-setting.entity';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, User, ProjectReport])],
+  imports: [TypeOrmModule.forFeature([Project, User, ProjectReport, SystemSetting])],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [AnalyticsService],
